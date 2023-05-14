8427afbcb259837cf5bee145dbc2d1bb750fd416
deep-son <deepanshusonparote@gmail.com>
2023-05-14 00:53:56 +0000

    Deploying to gh-pages from @ deep-son/portfolio@0c01b77b89b2e9bf23395cdf12dedc955048b88d 🚀

---
+++ precache-manifest.d811fa2025aef9c25b35a80dd5afa630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cefa071bd08cef0121db47e1d5b9864",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e77ed8f561f06cfb006a",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d23e79547ba386fe838e",
+    "url": "/portfolio/static/css/main.24206d80.chunk.css"
+  },
+  {
+    "revision": "e77ed8f561f06cfb006a",
+    "url": "/portfolio/static/js/2.65647c26.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.65647c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23e79547ba386fe838e",
+    "url": "/portfolio/static/js/main.3f0d02a3.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
